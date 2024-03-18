9c424b4701b3434d35cdfd7c022169cb02e48838
bicho <dearteaga.5243@unicesmag.edu.co>
2024-03-18 11:05:17 -0500

    base datos

--- app/academics/models.py
+++ app/academics/models.py
@@ -17,8 +17,13 @@ class User(models.Model):
 class Person(models.Model):
     firstname = models.CharField(max_length = 20) 
     lastname = models.CharField(max_length = 20)
+    id_ident_type = models.IntegerField()
     age = models.IntegerField()
+    ident_exp_city = models.IntegerField()
+    address = models.CharField(max_length = 150)
+    mobile = models.CharField(max_length = 50)
     ident_number = models.CharField(max_length = 12, blank = True) # blank = true que acaepte valores null
+    id_user = models.IntegerField()
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
@@ -28,15 +33,6 @@ class Student(models.Model):
     code = models.CharField(max_length = 50) 
     id_person = models.IntegerField()
     status = models.BooleanField(null = True, blank = True, default = True) 
-    created_at = models.DateTimeField(default = datetime.datetime.now())
-    updated_at = models.DateTimeField(default = datetime.datetime.now())
-    deleted_at = models.DateTimeField(null = True, blank = True)
-
-
-class Identification_type(models.Model):
-    name = models.CharField(max_length = 50)
-    abrev = models.CharField(max_length = 10)  
-    descrip = models.CharField(max_length = 100) 
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
